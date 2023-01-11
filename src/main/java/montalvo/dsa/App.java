efb8e8d1074c5bf40cc9c43d0511bcfe8737ce6d
amontalvof <amontalvof92@gmail.com>
2023-01-11 13:25:10 -0600

    feat: added Merge sort

---
+++ src/main/java/montalvo/dsa/App.java
@@ -0,0 +1,6 @@
+package montalvo.dsa;
+
+public class App {
+    public static void main(String[] args) {
+    }
+}
---
+++ src/main/java/montalvo/dsa/algorithms/sort/MergeSort.java
@@ -0,0 +1,65 @@
+package montalvo.dsa.algorithms.sort;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+// Write a class called MergeSort with a sort method which accepts an array and sorts it using the merge sort algorithm. Big O: O(n log n)
+public class MergeSort {
+
+    private static List<Integer> merge(List<Integer> arr1, List<Integer> arr2) {
+        List<Integer> results = new ArrayList<Integer>();
+        int i = 0;
+        int j = 0;
+        while (i < arr1.size() && j < arr2.size()) {
+            if (arr2.get(j) > arr1.get(i)) {
+                results.add(arr1.get(i));
+                i++;
+            } else {
+                results.add(arr2.get(j));
+                j++;
+            }
+        }
+        while (i < arr1.size()) {
+            results.add(arr1.get(i));
+            i++;
+        }
+        while (j < arr2.size()) {
+            results.add(arr2.get(j));
+            j++;
+        }
+        return results;
+    }
+
+    public static List<Integer> sort(List<Integer> arr) {
+        if (arr.size() <= 1) {
+            return arr;
+        }
+        int mid = arr.size() / 2;
+        List<Integer> left = sort(arr.subList(0, mid));
+        List<Integer> right = sort(arr.subList(mid, arr.size()));
+        return merge(left, right);
+    }
+
+    public static void main(String[] args) {
+        List<Integer> list = generateRandomList();
+        long start = System.nanoTime();
+        List<Integer> result = sort(list);
+        long end = System.nanoTime();
+        double elapsedTimeInSecond = (end - start) / 1_000_000_000.0;
+        System.out.println(result);
+        System.out.println("[46m************************************[0m");
+        System.out.println("Elapsed time in seconds: " + elapsedTimeInSecond);
+
+    }
+
+    private static List<Integer> generateRandomList() {
+        // List<Integer> list = Arrays.asList(10, 24, 76, 73, 72, 1, 9);
+        List<Integer> list = new ArrayList<Integer>();
+        Random rand = new Random();
+        for (int i = 0; i < 100000; i++) {
+            list.add(rand.nextInt());
+        }
+        return list;
+    }
+}
