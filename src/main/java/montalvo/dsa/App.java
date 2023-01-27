76fe252d0f66bba63849fdba1004423945c6b21e
amontalvof <amontalvof92@gmail.com>
2023-01-27 00:23:21 -0600

    feat: finished Single Linked List

--- src/main/java/montalvo/dsa/App.java
+++ src/main/java/montalvo/dsa/App.java
@@ -4,11 +4,12 @@ import montalvo.dsa.dataStructures.linkedLists.single.SingleLinkedList;
 
 public class App {
     public static void main(String[] args) {
-        SingleLinkedList<String> list = new SingleLinkedList<String>();
+        SingleLinkedList<String> list = new SingleLinkedList<>();
+        list.push("Lorem");
+        list.push("ipsum");
+        list.push("dolor");
+        System.out.print("\033[H\033[2J");
         System.out.println(list);
-        list.push("Hello");
-        System.out.println(list);
-        list.push("World");
-        System.out.println(list);
+        System.out.println(list.reverse());
     }
 }
